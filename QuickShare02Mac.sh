2f199eb0a6f4a1e77883bee56443a59e2a5c82f2
JordyVision <Sonof8Bits@users.noreply.github.com>
2015-11-17 21:10:52 +0100

    Update QuickShare02Mac.sh

--- QuickShare02Mac.sh
+++ QuickShare02Mac.sh
@@ -7,8 +7,8 @@
 ## or social media.
 ##
 ## Requirements:
-## You enjoy using the command line.
-## A Mac OSX computer.
+## - You enjoy using the command line.
+## - A computer running Mac OSX.
 ## - In .profile make an alias for QuickShare:
 ##   alias quickshare=~/script/QuickShare.sh
 ##   (Or don't, but typing the path to a script makes QuickShare a little
